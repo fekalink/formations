fe9679f2271d829f846741502340f31d3a575b5f
Guillaume Storchi <gstorchi@gmail.com>
2018-12-18 14:09:42 +0100

    enoncé exercice

---
+++ javascript/array/for.js
@@ -0,0 +1,43 @@
+//En tant que developpeur je veux ajouter une note à une matière
+
+
+let notes =  { 
+     "math" : [],
+     "francais" : [12, 13],
+     "informatique" : []
+};
+
+notes["math"].push(12);
+notes["math"].push(16);
+
+console.log(notes);
+
+notes["math"][0] = 11;
+
+console.log(notes);
+let notes =  { 
+  "math" : [],
+  "francais" : [12, 13],
+  "informatique" : []
+}; 
+
+function addNoteMatiere(matiere, note) {
+  for (nomMatiere in notes) {
+    if (nomMatiere == matiere) { 
+      notes[nomMatiere].push(note);
+    }
+  }
+
+}
+
+
+addNoteMatiere("informatique", 14);
+addNoteMatiere("informatique", 16);
+addNoteMatiere("math", 16);
+addNoteMatiere("math", 1);
+addNoteMatiere("francais", 19);
+console.log(notes); 
+
+
+
+
--- javascript/poo/eleveclass.js
+++ javascript/poo/eleveclass.js
@@ -19,29 +19,76 @@ class Personne {
   setNom(nom) {
     this.nom = nom;
   }
- 
+
+
+
+  
 }
+
+
+var notes = {
+  "math": [],
+  "anglais": []
+};
+
+//En tant que developpeur je veux pouvoir ajouter une note pour une matiere donnée
+//En tant que developpeur je veux pouvoir ajouter une coefficient à une matière
+//En tant que developpeur je veux pouvoir récupérer le coefficient d'une matière donnée, par défaut 1
+//En tant que developpeur je veux pouvoir calculer la moyenne pondérée pour un élève
 
 
 class Eleve extends Personne 
 {
-  
+
    constructor() {
       this.cursus = "";
+      this.notes = { "math":[], 
+                     "anglais":[], 
+                     "info":[]  
+                   }; 
    }
 
+   /**
+    * calcule de la moyenne pondérée
+    * @return Number moyenne
+    */
    getMoyenne() {
      let moyenne = 0;
      //calcul
      return moyenne;
    }
-   
+ 
+   getNotes() {
+    return this.notes;
+   }
+
+   /**
+    *@param notes objet notes
+    *
+    */
+   setNotes(notes) {
+    this.notes = notes;
+   }
+
+   /**
+    * Permet d'ajouter une note à une matière donnée
+    * @param String matiere
+    * @param Number note
+    */
+   addNoteMatiere(matiere, note) {
+       for ( nomMatiere in this.notes ) {
+           if ( nomMatiere === matiere ) {
+              this.notes[nomMatiere].push(note);
+           } 
+       }
+   }
 
 }
 
 var p = new Personne();
 var e = new Eleve();
 e.setNom("Marion Cotillard");
+e.getNotes();
 console.log("Nom " + e.getNom());
 
 

0b15182c9e9b0de5fa2e6a5b56100a1bd848c1ba
Guillaume Storchi <gstorchi@gmail.com>
2018-12-19 11:36:27 +0000

    moyenne générale et traitement des erreurs

--- javascript/poo/eleveclass.js
+++ javascript/poo/eleveclass.js
@@ -1,7 +1,7 @@
 //En tant que developpeur je veux pouvoir ajouter une note pour une matiere donnée
 //En tant que developpeur je veux pouvoir ajouter une coefficient à une matière
 //En tant que developpeur je veux pouvoir récupérer le coefficient d'une matière donnée, par défaut 1
-//En tant que developpeur je veux pouvoir calculer la moyenne pondérée pour un élève
+//En tant que developpeur je veux pouvoir calculer la moyenne générale pondérée pour un élève
 
 
 class Personne {
@@ -16,6 +16,16 @@ class Personne {
     //instructions appellée par new Personne()
     this.nom = "";
     this.prenom = "";
+    this.notes = {
+        "math":[],
+        "francais":[],
+        "info":[]
+    };
+    this.coeff = {
+        "math": 1,
+        "francais": 1,
+        "info": 1
+    };
   }
   //méthodes
   getNom() {
@@ -25,31 +35,30 @@ class Personne {
   setNom(nom) {
     this.nom = nom;
   }
- 
+
+
 }
 
-class Eleve extends Personne 
+
+class Eleve extends Personne
 {
 
    constructor(nom, prenom, dateDeNaissance) {
       super(nom, prenom, dateDeNaissance);
       this.cursus = "";
-      this.notes = { "math":[], 
-                     "anglais":[], 
-                     "info":[]  
-                   }; 
+      this.notes = {
+                     "math": [],
+                     "anglais":[],
+                     "info":[]
+                   };
+
+      this.coeff  = {
+          "math": 1,
+          "anglais": 1,
+          "info": 1
+      }
    }
 
-   /**
-    * calcule de la moyenne pondérée
-    * @return Number moyenne
-    */
-   getMoyenne() {
-     let moyenne = 0;
-     //calcul
-     return moyenne;
-   }
- 
    getNotes() {
     return this.notes;
    }
@@ -59,7 +68,7 @@ class Eleve extends Personne
     *
     */
    setNotes(notes) {
-    this.notes = notes;
+     this.notes = notes;
    }
 
    /**
@@ -68,17 +77,95 @@ class Eleve extends Personne
     * @param Number note
     */
    addNoteMatiere(matiere, note) {
-       let nomMatiere;
-       for (nomMatiere in this.notes ) {
-           if ( nomMatiere === matiere ) {
-              this.notes[nomMatiere].push(note);
-           } 
+     let nomMatiere;
+     for (nomMatiere in this.notes ) {
+       if ( nomMatiere === matiere ) {
+         this.notes[nomMatiere].push(note);
        }
+     }
    }
+
+   getNotesMatiere(matiere) {
+     let notesMatiere = this.notes[matiere];
+     //traitement de l'erreur
+     if ( notesMatiere == undefined ) {
+       throw "[ERROR] La matiere " + matiere +
+       " n'existe pas dans le tableau des notes : " +
+       JSON.stringify(this.getNotes()) ;
+     }
+     console.log(notesMatiere);
+     return notesMatiere;
+   }
+
+   getCoeff() {
+     return this.coeff;
+   }
+
+   setCoeff(coeff) {
+     this.coeff = coeff;
+   }
+
+
+   setCoeffMatiere(matiere, coeff) {
+     for(let nomMatiere in this.coeff) {
+       if(nomMatiere == matiere) {
+         this.coeff[nomMatiere] = coeff;
+       }
+     }
+   }
+
+   getCoeffMatiere(matiere) {
+      return this.coeff[matiere];
+   }
+
+   getSommeCoeff() {
+      let sommeCoeff = 0;
+      let coeff = this.getCoeff();
+      for (let matiere in coeff) {
+        sommeCoeff += coeff[matiere];
+      }
+      return sommeCoeff;
+   }
+
+   getMoyenneMatiere(matiere) {
+     //https://putaindecode.io/fr/articles/js/array-reduce/
+     let notesMatiere;
+     let somme;
+     try {
+       notesMatiere = this.getNotesMatiere(matiere);
+       somme = notesMatiere.reduce(  function(memo, val) {
+                                       return memo + val;
+                                     }
+                                  );
+     } catch(error) {
+        console.log("[ERREUR] Problème d'initialisation du tableau de notes " +
+                    "pour la matiere " + matiere);
+     }
+     let nombreDeNotes = notesMatiere.length;
+     let moyenne = somme / nombreDeNotes;
+     return moyenne;
+   }
+
+   /**
+    * Calcul de la moyenne pondérée de toutes les matières
+    */
+   getMoyenneGenerale() {
+      let moyenneGenerale = 0;
+      let notes = this.getNotes();
+      let sommeDesCoefficients = 0;
+      for (let matiere in notes) {
+        let coefficientMatiere = this.getCoeffMatiere(matiere);
+        let moyenneMatiere = this.getMoyenneMatiere(matiere)*coefficientMatiere;
+        moyenneGenerale = moyenneGenerale + moyenneMatiere;
+      }
+      sommeDesCoefficients = this.getSommeCoeff();
+      moyenneGenerale = moyenneGenerale / sommeDesCoefficients;
+      return moyenneGenerale;
+   }
+
 
 }
 
-var p = new Personne();
 
 var e = new Eleve();
 e.setNom("Cotillard");
@@ -87,13 +174,18 @@ e.addNoteMatiere("math", 12);
 e.addNoteMatiere("math", 11);
 e.addNoteMatiere("math", 17);
 e.addNoteMatiere("anglais", 11);
+e.addNoteMatiere("info", 11);
 
+e.setCoeffMatiere("math", 5);
+console.log("[DEBUG] Liste des coefficients : ", e.getCoeff());
+console.log("[DEBUG] Coeffcieint de math : ", e.getCoeffMatiere("math"));
+console.log("[DEBUG] Moyenne des notes de maths =" +  e.getMoyenneMatiere("mathssss"));
 
 var  notes = e.getNotes();
-console.log("Nom " + e.getNom());
-console.log("Notes : " + JSON.stringify(e.getNotes()));
-
-
+console.log("[DEBUG] Nom " + e.getNom());
+console.log("[DEBUG] Notes : " + JSON.stringify(e.getNotes()));
 console.log(notes);
 
+console.log("[DEBUG] Somme des coeff: " + e.getSommeCoeff());
 
+console.log("[DEBUG] Moyenne Generale = " +e.getMoyenneGenerale());

7ac25b58a9604a53f0190a86710eefc18d6f994d
Guillaume Storchi <gstorchi@gmail.com>
2019-01-07 09:12:18 +0000

    modal signup gloabe au site

--- javascript/website/modules/menu/js/05-menu.js
+++ javascript/website/modules/menu/js/05-menu.js
@@ -23,4 +23,40 @@ window.addEventListener("load", function() {
     document.getElementById("menu").appendChild(a);
   }
 
+  /****************/
+  /* SIGNUP MODAL */
+  /****************/
+  settings = {
+    async: true,
+    data: {},
+    targetSelector: "#modal-signup-form-container"
+  };
+  MUSICBAND.query.get("/modules/users/modal-signup.html", settings, function(responseText, settings) {
+    console.log(settings);
+    document.querySelector(settings.targetSelector).innerHTML = responseText;
+    //click sur signup
+    document.getElementById("signup-link").addEventListener("click", function(e) {
+      document.getElementById('modal-signup-form').style.display = "block";
+      e.stopPropagation();
+      e.preventDefault();
+    });
+    //fermeture de la modale
+    document.querySelectorAll('#modal-signup-form .close').forEach(function(node) {
+      node.addEventListener('click', function (e) {
+        document.getElementById('modal-signup-form').style.display = 'none';
+      })
+    });
+    //soumission signup form
+    document.getElementById('user-signup-form').addEventListener('submit', function(e) {
+      e.stopPropagation();
+      let formData = new FormData(this);
+      let settings = {
+        async:false,
+        data: formData
+      };
+      MUSICBAND.query.post("/modules/ajax/05-controller.php?action=createUser", settings, function(responseText, settings) {
+      } );
+    })
+  });
+
 });
--- javascript/website/modules/users/js/05-users.js
+++ javascript/website/modules/users/js/05-users.js
@@ -50,43 +50,5 @@ window.addEventListener("load", function() {
       e.stopPropagation();
       e.preventDefault();
     });
-  });
-
-  /****************/
-  /* SIGNUP MODAL */
-  /****************/
-  let settings = {
-    async: true,
-    data: {},
-    targetSelector: "#modal-signup-form-container"
-  };
-  MUSICBAND.query.get("/modules/users/modal-signup.html", settings, function(responseText, settings) {
-    console.log(settings);
-    document.querySelector(settings.targetSelector).innerHTML = responseText;
-    //click sur signup
-    document.getElementById("signup-link").addEventListener("click", function(e) {
-      document.getElementById('modal-signup-form').style.display = "block";
-      e.stopPropagation();
-      e.preventDefault();
-    });
-    //fermeture de la modale
-    document.querySelectorAll('#modal-signup-form .close').forEach(function(node) {
-      node.addEventListener('click', function (e) {
-        document.getElementById('modal-signup-form').style.display = 'none';
-      })
-    });
-    //soumission signup form
-    document.getElementById('user-signup-form').addEventListener('submit', function(e) {
-      e.stopPropagation();
-      let formData = new FormData(this);
-      let settings = {
-        async:false,
-        data: formData
-      };
-      MUSICBAND.query.post("/modules/ajax/05-controller.php?action=createUser", settings, function(responseText, settings) {
-      } );
-    })
-  });
-
-
+  });  
 });
